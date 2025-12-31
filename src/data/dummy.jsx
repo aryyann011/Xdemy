8d10ce970f3cfaaeb7a83fbde0f7573a3b18d827
Aryan Mishra <aryan456.rehla@gmail.com>
2025-12-31 12:29:53 +0530

    Line chart

--- src/data/dummy.jsx
+++ src/data/dummy.jsx
@@ -758,39 +758,13 @@ export const scheduleData = [
 // ----------------------------------------------------------------------
 
 export const lineChartData = [
-  [
-    { x: new Date(2005, 0, 1), y: 21 },
-    { x: new Date(2006, 0, 1), y: 24 },
-    { x: new Date(2007, 0, 1), y: 36 },
-    { x: new Date(2008, 0, 1), y: 38 },
-    { x: new Date(2009, 0, 1), y: 54 },
-    { x: new Date(2010, 0, 1), y: 57 },
-    { x: new Date(2011, 0, 1), y: 70 },
-  ],
-  [
-    { x: new Date(2005, 0, 1), y: 28 },
-    { x: new Date(2006, 0, 1), y: 44 },
-    { x: new Date(2007, 0, 1), y: 48 },
-    { x: new Date(2008, 0, 1), y: 50 },
-    { x: new Date(2009, 0, 1), y: 66 },
-    { x: new Date(2010, 0, 1), y: 78 },
-    { x: new Date(2011, 0, 1), y: 84 },
-  ],
-  [
-    { x: new Date(2005, 0, 1), y: 10 },
-    { x: new Date(2006, 0, 1), y: 20 },
-    { x: new Date(2007, 0, 1), y: 30 },
-    { x: new Date(2008, 0, 1), y: 39 },
-    { x: new Date(2009, 0, 1), y: 50 },
-    { x: new Date(2010, 0, 1), y: 70 },
-    { x: new Date(2011, 0, 1), y: 100 },
-  ],
-];
-
-export const lineCustomSeries = [
-  { dataSource: lineChartData[0], xName: 'x', yName: 'y', name: 'Course A Sales', width: '2', marker: { visible: true, width: 10, height: 10 }, type: 'Line' },
-  { dataSource: lineChartData[1], xName: 'x', yName: 'y', name: 'Course B Sales', width: '2', marker: { visible: true, width: 10, height: 10 }, type: 'Line' },
-  { dataSource: lineChartData[2], xName: 'x', yName: 'y', name: 'Course C Sales', width: '2', marker: { visible: true, width: 10, height: 10 }, type: 'Line' },
+  { x: '2005', CourseA: 21, CourseB: 28, CourseC: 10 },
+  { x: '2006', CourseA: 24, CourseB: 44, CourseC: 20 },
+  { x: '2007', CourseA: 36, CourseB: 48, CourseC: 30 },
+  { x: '2008', CourseA: 38, CourseB: 50, CourseC: 39 },
+  { x: '2009', CourseA: 54, CourseB: 66, CourseC: 50 },
+  { x: '2010', CourseA: 57, CourseB: 78, CourseC: 70 },
+  { x: '2011', CourseA: 70, CourseB: 84, CourseC: 100 },
 ];
 
 export const areaChartData = [
